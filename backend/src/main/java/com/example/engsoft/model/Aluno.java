a851d17aada421967bb966601843321689339093
Gabriel Diniz <bieldinizsoares2016@gmail.com>
2025-08-28 23:47:35 -0300

    aluno recurso

--- backend/src/main/java/com/example/engsoft/model/Aluno.java
+++ backend/src/main/java/com/example/engsoft/model/Aluno.java
@@ -1,11 +1,30 @@
 package com.example.engsoft.model;
 
-
-import jakarta.persistence.Entity;
+import com.example.engsoft.model.Disciplina;
+import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
+@Table(name = "tb_aluno")
 @Data
-public class Aluno extends Usuario{
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String matricula;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "rl_aluno_disciplina",
+            joinColumns = @JoinColumn(name = "id_aluno"),
+            inverseJoinColumns = @JoinColumn(name = "id_disciplina")
+    )
+    private List<Disciplina> disciplinas;
 }
