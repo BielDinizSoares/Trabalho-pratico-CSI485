dac651f7855dfbfa636096ea04abe7018473924d
Gabriel Diniz <bieldinizsoares2016@gmail.com>
2025-08-28 23:47:08 -0300

    disciplina recurso

--- backend/src/main/java/com/example/engsoft/model/Disciplina.java
+++ backend/src/main/java/com/example/engsoft/model/Disciplina.java
@@ -1,17 +1,31 @@
 package com.example.engsoft.model;
 
+import com.example.engsoft.model.Professor;
+import com.example.engsoft.model.Tarefa;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
+@Table(name = "tb_disciplina")
 @Data
 public class Disciplina {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
+    private String name;
+    private Integer cargaHoraria;
 
     @ManyToOne
+    @JoinColumn(name = "id_professor")
     private Professor professor;
+
+    @OneToMany(mappedBy = "disciplina", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tarefa> tarefas;
+
+    @ManyToMany(mappedBy = "disciplinas")
+    private List<Aluno> alunos;
 }
